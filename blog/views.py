ec2b9f08b191763667bbf81731053ec84a98e257
Qais Saleh Bsharat <kaisbsharat@gmail.com>
2018-06-15 10:09:57 +0300

    first commit

---
+++ blog/views.py
@@ -0,0 +1,101 @@
+from django.shortcuts import render, get_object_or_404, redirect
+from django.utils import timezone
+from django.urls import reverse_lazy  # Or from django.core.urlresolvers import reverse_lazy
+from blog.forms import PostForm, CommentForm
+from blog.models import Post, Comment
+from django.views.generic import (TemplateView, ListView, DetailView, CreateView, UpdateView, DeleteView)
+
+# This is for CBV but for function based view we was used: from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.decorators import login_required
+
+
+# Create your views here.
+
+class AboutView(TemplateView):
+    template_name = 'about.html'
+
+
+class PostListView(ListView):
+    model = Post
+
+    def get_queryset(self):
+        """
+          This method to write sql query
+          lte:less than or equal (more info : https://docs.djangoproject.com/en/2.0/topics/db/queries/#field-lookups)
+         -published_date : without - will see the oldest blog post first with a (-) mean descending order
+        """
+        return Post.objects.filter( published_date__lte=timezone.now() ).order_by( '-published_date' )
+
+
+class PostDetailView( DetailView ):
+    model = Post
+
+
+class CreatePostView( LoginRequiredMixin, CreateView ):
+    login_url = '/login/'
+    redirect_field_name = 'blog/post_detail.html'
+    form_class = PostForm
+    model = Post
+
+
+class PostUpdateView( LoginRequiredMixin, UpdateView ):
+    login_url = '/login/'
+    redirect_field_name = 'blog/post_detail.html'
+    form_class = PostForm
+    model = Post
+
+
+class PostDeleteView( LoginRequiredMixin, DeleteView ):
+    model = Post
+    success_url = reverse_lazy( 'post_list' )
+    login_url = '/login/'
+
+
+class DraftListView( LoginRequiredMixin, ListView ):
+    login_url = '/login/'
+    redirect_field_name = 'blog/post_list.html'
+    model = Post
+
+    def get_queryset(self):
+        return Post.objects.filter( published_date__isnull=True ).order_by( 'created_date' )
+
+
+################################
+################################
+
+@login_required
+def post_publish(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.publish()
+    return redirect('post_detail', pk=pk)
+
+
+def add_comment_to_post(request, pk):
+    # Get the object Or 404 error in this case (Get The Post Object Or 404 Error)
+    post = get_object_or_404( Post, pk=pk )
+    if request.method == "POST":
+        form = CommentForm( request.POST )
+        if form.is_valid():
+            comment = form.save( commit=False )
+            comment.post = post
+            comment.save()
+            return redirect( 'post_detail', pk=post.pk )
+    else:
+        form = CommentForm()
+    return render( request, 'blog/comment_form.html', {'form': form} )
+
+
+@login_required
+def comment_approve(request, pk):
+    comment = get_object_or_404(Comment, pk=pk)
+    comment.approve()
+    return redirect('post_detail', pk=comment.post.pk)
+
+
+@login_required
+def comment_remove(request, pk):
+    comment = get_object_or_404(Comment, pk=pk)
+    post_pk = comment.post.pk
+    comment.delete()
+    return redirect('post_detail', pk=post_pk)
